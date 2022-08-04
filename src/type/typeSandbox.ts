bffd2b7ca0b168a5c84715df8d7bd74c09424919
JamieVaughn <wxv6@case.edu>
2022-08-04 19:02:31 -0400

    Add ts tip #3

--- src/type/typeSandbox.ts
+++ src/type/typeSandbox.ts
@@ -47,3 +47,25 @@ const result: EntityWithId = {
   type: "comment",
   commentId: "123",
 }
+
+// Tip #3: Union merge query params at the type level
+import { String, Union } from 'ts-toolbelt'
+
+const query = `/home?a=wow&b=woah`
+
+type Query = typeof query
+
+type SecondQueryPart = String.Split<Query, "?">[1]
+
+type QueryElements = String.Split<SecondQueryPart, "&">
+
+type QueryParams = {
+  [QueryElement in QueryElements[number]]: {
+    [Key in String.Split<QueryElement, '='>[0]]: String.Split<QueryElement, '='>[1];
+  }
+}[QueryElements[number]]
+
+const obj: Union.Merge<QueryParams> = {
+  a: "wow",
+  b: "woah",
+}
