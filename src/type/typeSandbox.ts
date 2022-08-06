f00ff6cac26dc6d11a781b98e4ac9bb6fac3f3ae
JamieVaughn <wxv6@case.edu>
2022-08-05 23:30:39 -0400

    Some typescript practice

--- src/type/typeSandbox.ts
+++ src/type/typeSandbox.ts
@@ -65,7 +65,77 @@ type QueryParams = {
   }
 }[QueryElements[number]]
 
-const obj: Union.Merge<QueryParams> = {
+export const obj: Union.Merge<QueryParams> = {
   a: "wow",
   b: "woah",
 }
+
+
+// Tip #4: function overloads to compose functions
+export function compose<Input, FirstArg>(
+  func: (Input: Input) => FirstArg
+): (Input: Input) => FirstArg
+
+export function compose<Input, FirstArg, SecondArg>(
+  func: (Input: Input) => FirstArg,
+  func1: (Input: FirstArg) => SecondArg
+): (Input: Input) => SecondArg
+
+export function compose<Input, FirstArg, SecondArg, ThirdArg>(
+  func: (Input: Input) => FirstArg,
+  func1: (Input: FirstArg) => SecondArg,
+  func2: (Input: SecondArg) => ThirdArg
+): (Input: Input) => ThirdArg
+
+export function compose(...args: any[]) {
+  return {} as any
+}
+
+const addOne = (a: number): number => a + 1
+const numToString = (a:number) => a.toString()
+const stringToNum = (a: string) => parseInt(a)
+const addOneToString = compose(addOne, numToString, stringToNum) // Will error if you pass in args in wrong order
+// kind of like elixir multivariadic functions
+
+//Tip #5: extends keywrod for narrowing value of a generic type
+
+export const getDeepValue = <
+  Obj, 
+  FirstKey extends keyof Obj, 
+  SecondKey extends keyof Obj[FirstKey]
+> (obj: Obj, firstKey: FirstKey, secondKey: SecondKey): Obj[FirstKey][SecondKey]  => {
+  return {} as any
+}
+
+const myObj = {
+  foo: {
+    a: true,
+    b: 'wow'
+  },
+  bar: {
+    c: 'cool',
+    d: 3
+  }
+}
+
+const objResult = getDeepValue(myObj, 'foo', 'b')
+
+
+// Tip #6: it's in vizbin because of React dependency
+
+
+// Tip #7 Generics
+
+export const myObject = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+const objectKeys = <Obj>(obj: Obj): (keyof Obj)[] => {
+  return Object.keys(obj) as (keyof Obj)[]
+}
+
+Object.keys(myObject).forEach((key) => {
+  console.log(myObject[key])
+})
